297e68b19aa1f45975351d13a0402dc076c42842
Francis Kayiwa <fkayiwa@colgate.edu>
2014-01-12 10:33:55 -0500

    south added for migration

--- inventory/inventory/settings/base.py
+++ inventory/inventory/settings/base.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import sys
+from os.path import abspath, basename, dirname, join, normpath
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
